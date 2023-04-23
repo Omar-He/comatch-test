fbaa1b21a478d6fbbeaa642b25fa5f665b1c1b20
Omar-He <o.hande@propertyexpert.de>
2023-04-23 13:13:22 +0300

    update

--- src/helpers/requests.ts
+++ src/helpers/requests.ts
@@ -1,13 +1,13 @@
 export async function getProjects(pageNumber: number, limit: number) {
   const req = fetch(
-    `http://localhost:7000/projects?pageNumber=${pageNumber}&limit=${limit}`
+    `http://localhost:3001/projects?pageNumber=${pageNumber}&limit=${limit}`
   );
   const res = await req;
   return res.json();
 }
 
 export async function getProjectByID(id: string) {
-  const req = fetch(`http://localhost:7000/projects/${id}`);
+  const req = fetch(`http://localhost:3001/projects/${id}`);
   const res = await req;
   return res.json();
 }
